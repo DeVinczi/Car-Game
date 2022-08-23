2c5572cec4580f0fe9d85cb7ee73cd7df27c5e5e
Dawid Slysz <dawid8sly@gmail.com>
2022-08-23 15:57:30 +0200

    Zmienne globalne i funkcja przyjmujaca x i y

--- Car Game/Main.cpp
+++ Car Game/Main.cpp
@@ -1,6 +1,39 @@
-#include <stdio.h>
+#include <iostream>
+#include <conio.h>
+#include <dos.h>
+#include <Windows.h>
+#include <time.h>
 
+#define SCREEN_WIDTH 90
+#define SCREEN_HEIGHT 30
+#define WIN_WIDTH 70
 
+using namespace std;
+
+HANDLE console = GetStdHandle(STD_OUTPUT_HANDLE);
+COORD CursorPosition;
+
+//GLOBAL
+int enemyY[3];
+int enemyX[3];
+int enemyFlag[3];
+//CAR IN CONSOLE 
+char car[4][4] =
+{
+	' ','+','+',' ',
+	'+','+','+','+',
+	' ','+','+',' ',
+	'+','+','+','+'
+};
+int carPos = WIN_WIDTH / 2;
+int score = 0;
+
+void gotoxy(int x, int y)
+{
+	CursorPosition.X = x;
+	CursorPosition.Y = y;
+	SetConsoleCursorPosition(console, CursorPosition);
+}
 
 int main()
 {
